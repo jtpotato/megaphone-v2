d49efccc8a27397af710dad20f5ba11923e19ace
Joel <58995538+jtpotato@users.noreply.github.com>
2022-12-18 09:10:45 +1100

    try to style sign-up page

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Card from '$lib/ui/boba-ui/Card.svelte';
+    import Text from '$lib/ui/boba-ui/Text.svelte';
+</script>
+
+<div>
+	<div
+		class="bg-gradient-to-bl from-orange-300 to-purple-300 w-screen h-screen -z-10 absolute top-0 left-0"
+	/>
+	<Card glass>
+        <div class="flex content-center justify-center flex-col">
+            <Text variant="h2" class="text-center">
+                Sign Up!
+            </Text>
+            <Text class="text-center">
+                We've got some cool stuff here. We hope.
+            </Text>
+        </div>
+
+    </Card>
+</div>

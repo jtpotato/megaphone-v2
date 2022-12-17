f75b924f058a11b4d40af023b28d5819012a6372
Joel <58995538+jtpotato@users.noreply.github.com>
2022-12-17 15:32:38 +1100

    add card feature

---
+++ src/lib/ui/boba-ui/Post.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Card from './Card.svelte';
+	import Text from './Text.svelte';
+
+	export let profilePictureURL: string;
+	export let profileName: string;
+	export let profileHandle: string = '';
+</script>
+
+<!--Built on Card.svelte, designed for Twitter-esque posts-->
+
+<Card>
+	<div class="flex items-center gap-2">
+		<img src={profilePictureURL} alt="A Profile" class="h-12 w-12 rounded-full" />
+		<div>
+			<p>
+				{profileName}
+			</p>
+			<p>{profileHandle}</p>
+		</div>
+	</div>
+	<div class="ml-14">
+		<slot />
+	</div>
+</Card>
--- src/routes/signedout/+page.svelte
+++ src/routes/signedout/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	import Text from "$lib/ui/boba-ui/Text.svelte";
+    import Post from "$lib/ui/boba-ui/Post.svelte";
+</script>
 <div>
-    <p>You're not signed in, but that's okay. You can still check out the cool stuff here.</p>
+    <Text>You're not signed in, but that's okay. You can still check out the cool stuff here.</Text>
+    <Text>Top Shouts:</Text>
+    <Post profileName="John Citizen" profileHandle="@jcitizen" profilePictureURL="https://pbs.twimg.com/profile_images/1469062206377582600/8uH6FfBy_400x400.png">
+        This is to test if the Post feature works.
+    </Post>
 </div>

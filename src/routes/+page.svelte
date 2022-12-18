cbdbb42f4b5a4a9274e80bc0c51b0354aef8b4ca
Joel <58995538+jtpotato@users.noreply.github.com>
2022-12-18 20:24:10 +1100

    include a terms of service

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,25 @@
 <script>
-	import InternalLink from '$lib/ui/boba-ui/InternalLink.svelte';
+	import Link from '$lib/ui/boba-ui/ExternalLink.svelte';
+	import Text from '$lib/ui/boba-ui/Text.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>About</title>
+	<meta name="description" content="About this app" />
 </svelte:head>
 
 <div>
-	TODO: REMOVE BEFORE PROD Signed out page: <InternalLink href="/signedout"
-		>Signed out page</InternalLink
-	>
+	<Text variant="h1">We're Megaphone 📢</Text>
+	<Text>
+		As our name implies, our mission is to give people a voice. We also know that the internet can
+		be a toxic, dangerous place. While allowing people to be heard and creating a safe environment
+		sound like two conflicting goals, we are confident that it is possible. While our platform's
+		Terms of Service sound harsh, it's really just a verbose way of saying "please be nice".
+	</Text>
+	<Text>
+		At least, that's what we would say if we were serious about this. Megaphone is one of <Link
+			href="https://youtube.com/@jtpotato">my</Link
+		>
+		silly projects, and I doubt any of what I just said above is true. Do have a look around anyway.
+	</Text>
 </div>
-
-<style>
-</style>

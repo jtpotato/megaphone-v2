238c7c2c0987a8959a5c4a03d8a597d7dbd129cf
Joel <58995538+jtpotato@users.noreply.github.com>
2022-12-20 17:50:11 +1100

    auth works even better!

---
+++ src/routes/signedin/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+    import { isSignedIn } from "../stores";
+
+    let page_isSignedIn: boolean;
+    isSignedIn.subscribe((value) => { page_isSignedIn = value; });
+</script>
+
+<div>
+    {#if page_isSignedIn}
+        <slot />
+    {:else}
+        <script>
+            goto("/login")
+        </script>
+    {/if}
+</div>
---
+++ src/routes/signedin/onboarding/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Card from "$lib/ui/boba-ui/Card.svelte";
+	import Text from "$lib/ui/boba-ui/Text.svelte";
+</script>
+
+<Card glass>
+    <Text>Choose a tag</Text>
+    <Text>... and a username</Text>
+</Card>

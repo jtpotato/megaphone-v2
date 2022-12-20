d23444ff842631b0d8c3d75abd9d7817c988e6aa
Joel <58995538+jtpotato@users.noreply.github.com>
2022-12-20 21:56:47 +1100

    get onboarding working

--- src/routes/signedin/onboarding/+page.svelte
+++ src/routes/signedin/onboarding/+page.svelte
@@ -1,9 +1,39 @@
 <script>
-	import Card from "$lib/ui/boba-ui/Card.svelte";
-	import Text from "$lib/ui/boba-ui/Text.svelte";
+	import Button from '$lib/ui/boba-ui/Button.svelte';
+	import Card from '$lib/ui/boba-ui/Card.svelte';
+	import Text from '$lib/ui/boba-ui/Text.svelte';
+	import { getAuth } from 'firebase/auth';
+	import { doc, getFirestore, setDoc } from 'firebase/firestore';
+	import { goto } from '$app/navigation';
+
+	let handle = '';
+	let username = '';
+
+	const updateProfile = async () => {
+		const db = getFirestore();
+		const auth = getAuth();
+		if (auth.currentUser) {
+			await setDoc(doc(db, 'users', auth.currentUser.uid), {
+				handle: `${handle}`,
+				username: username
+			}).then(() => {
+				console.log('written to firebase!');
+				goto('/signedin');
+			});
+		}
+	};
 </script>
 
-<Card glass>
-    <Text>Choose a tag</Text>
-    <Text>... and a username</Text>
-</Card>
+<div>
+	<div
+		class="bg-gradient-to-bl from-orange-300 to-purple-300 w-screen h-screen -z-10 absolute top-0 left-0"
+	/>
+	<Card glass>
+		<p>Choose a handle</p>
+		<input bind:value={handle} />
+		<p>... and a username</p>
+		<input bind:value={username} />
+		<div class="h-6" />
+		<Button action={updateProfile}>Let's Go!</Button>
+	</Card>
+</div>

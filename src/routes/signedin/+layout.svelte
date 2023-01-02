e68e11e5bea3d9786e54b6549797b2b52ceb8191
Joel <58995538+jtpotato@users.noreply.github.com>
2023-01-02 22:11:52 +1100

    add a thing for when you're not signed in

--- src/routes/signedin/+layout.svelte
+++ src/routes/signedin/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import Button from '$lib/ui/boba-ui/Button.svelte';
 	import { redirect } from '@sveltejs/kit';
 	import { isSignedIn } from '../stores';
 
@@ -7,10 +8,17 @@
 	isSignedIn.subscribe((value) => {
 		page_isSignedIn = value;
 	});
+
+	const goHome = () => {
+		goto("/");
+	}
 </script>
 
 <div>
 	{#if page_isSignedIn}
 		<slot />
+	{:else}
+		<p>Aw snap, you don't appear to be signed in.</p>
+		<Button action={goHome}>Go Home</Button>
 	{/if}
 </div>
